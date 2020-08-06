ec690e5fafe157a3d238b02b98612a43f5b0c395
jorgevz <jorgev2132@gmail.com>
2020-08-06 10:02:46 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Web1 from './component/Web1';
+
+
+function App() {
+  return (
+    <div>
+     <Web1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Web1.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+
+class Web1 extends React.Component{
+render(){
+return(
+<div>
+<div className='red'/>
+<p id='t1'>Catalysts</p>
+<p id='t2'></p>
+<p id='t3'>ISSUES  TOPICS   BLOG  NEWSLETTER</p>
+<p id="t4">WHY THE LAWS OF PHYSICS ARE INEVITABLE?</p>
+<p id='t5'>Catalyst</p>
+<p id='t7'>Chapter one</p>
+<p id='t8'>Chapter two</p>
+<p id='t9'>Chapter three</p>
+<p id='t10'>Chapter four</p>
+<p id="t6"></p>
+<div id='box1'>
+<p>Artificial Intelligence</p>
+<p id='tb1'>Best screen play goes to the Algorithm</p>
+</div>
+<div id='box2'>
+<p id='t11'></p>
+<p ></p>
+</div>
+</div>
+)
+}
+
+
+
+}
+
+export default Web1; 
